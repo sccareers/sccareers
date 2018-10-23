ec143984ff4bd2ac90acb4597ebc237a87e5454e
Karolis Grigas <k.grigas@spectrofinance.lt>
2018-10-23 11:08:19 +0300

    Updates

---
+++ precache-manifest.346380ea8a0c0b59e58e4185594857d4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "472d97a823bc74eca3293c8198208d89",
+    "url": "/static/media/AvenirNextLTPro-Demi.472d97a8.otf"
+  },
+  {
+    "revision": "ad81cfe1da6da67c4bad",
+    "url": "/static/css/main.0c715001.chunk.css"
+  },
+  {
+    "revision": "9784ad298b33d201079f",
+    "url": "/static/js/1.9784ad29.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ae40eb7482ebe80967ac6a5f4be89b0",
+    "url": "/static/media/SCORE CIRCLE.8ae40eb7.svg"
+  },
+  {
+    "revision": "9c4bd03a2c8ce238867648d707f4b19c",
+    "url": "/static/media/circle.9c4bd03a.svg"
+  },
+  {
+    "revision": "65a2630423f2aed039d51c74355fc524",
+    "url": "/static/media/logo.65a26304.svg"
+  },
+  {
+    "revision": "ad81cfe1da6da67c4bad",
+    "url": "/static/js/main.ad81cfe1.chunk.js"
+  },
+  {
+    "revision": "b62f8f0b14aebd847dcc75a459018f69",
+    "url": "/static/media/AvenirNextLTPro-Regular.b62f8f0b.otf"
+  },
+  {
+    "revision": "0c56d9a5332c29a58e4c691eb5373d3d",
+    "url": "/static/media/bitcoinn.0c56d9a5.svg"
+  },
+  {
+    "revision": "83539177a068cd3f83affce89f282625",
+    "url": "/static/media/nem.83539177.svg"
+  },
+  {
+    "revision": "f33831de7ed79160a8c8f2991adbea42",
+    "url": "/static/media/ethereum.f33831de.svg"
+  },
+  {
+    "revision": "3ab4103f2e5a5c1251fd285aa070b375",
+    "url": "/static/media/dash.3ab4103f.svg"
+  },
+  {
+    "revision": "3e3545ef9a58c34d4f0c02a25aa32009",
+    "url": "/static/media/eos.3e3545ef.svg"
+  },
+  {
+    "revision": "efd19a2cd1b858a94071b33bb144a908",
+    "url": "/index.html"
+  }
+];
